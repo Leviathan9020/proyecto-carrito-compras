ad638070aff31759ab69657ddbdcc80087951f7c
Leviathan9020 <jomar2030@hotmail.com>
2023-04-12 15:16:16 -0500

    se sube proyecto de carrito de compras, falta arreglar error que aparece doble en el carrito

---
+++ js/app.js
@@ -0,0 +1,93 @@
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
+
+cargarEventListeners();
+function cargarEventListeners() {    
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+
+function agregarCurso(e) {
+    e.preventDefault();    
+    if(e.target.classList.contains('agregar-carrito')) {
+        const cursoSeleccionado = e.target.parentElement.parentElement; 
+        leerDatosCurso(cursoSeleccionado);
+    }
+   
+}
+
+function leerDatosCurso(curso) {
+    
+
+    
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+         
+     const existe = articulosCarrito.some( curso => curso.id === infoCurso.id );
+    if(existe) {
+        
+        const cursos = articulosCarrito.map( curso => {
+            if( curso.id === infoCurso.id ) {
+                curso.cantidad++;
+                return curso; 
+            }else {
+                return curso; 
+            }
+        });
+        articulosCarrito = [...cursos];
+    }else {
+        
+        articulosCarrito = [...articulosCarrito, infoCurso];
+    }
+
+    
+     articulosCarrito = [...articulosCarrito, infoCurso];
+     console.log(articulosCarrito)
+
+     carritoHTML();
+
+}
+
+
+function carritoHTML() {
+    
+    limpiarHTML();
+
+    
+    articulosCarrito.forEach( curso => {
+        const { imagen, titulo, precio, cantidad, id } = curso;
+        const row = document.createElement('tr');        
+        row.innerHTML = `
+            <td>
+                <img src="${imagen}" width= "100">
+            </td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${id}" > X </a>
+            </td>
+        `;
+
+        
+        contenedorCarrito.appendChild(row);
+    })
+}
+
+
+function limpiarHTML() {    
+
+    while(contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
+
+}
